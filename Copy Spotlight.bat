7028c88b412984c1da96a964b13802934e1fbb82
Jackson <CamoJackson@users.noreply.github.com>
2016-10-25 11:11:12 -0400

    Updated Save Location

--- Copy Spotlight.bat
+++ Copy Spotlight.bat
@@ -1,4 +1,4 @@
 xcopy %localappdata%\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets C:\Users\%Username%\Photos\"Windows Spotlight"
 cd \
-cd Users\%Username%\Photos\"Windows Spotlight"
+cd Users\%Username%\Pictures\"Windows Spotlight"
 ren * *.jpg
