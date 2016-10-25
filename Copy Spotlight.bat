c5fee78ff12515c9bb8bde2fefdcf9a98c2e9af9
Jackson <CamoJackson@users.noreply.github.com>
2016-10-25 11:08:45 -0400

    Added Base Files

---
+++ Copy Spotlight.bat
@@ -0,0 +1,4 @@
+xcopy %localappdata%\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets C:\Users\%Username%\Photos\"Windows Spotlight"
+cd \
+cd Users\%Username%\Photos\"Windows Spotlight"
+ren * *.jpg
