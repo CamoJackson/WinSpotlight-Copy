fe9cc157510c4944e5a69fd4a9fea06dcce59bc8
Jackson <jackson@borneman.com>
2016-10-25 11:32:10 -0400

    Fixed XCopy

--- Copy Spotlight.bat
+++ Copy Spotlight.bat
@@ -1,4 +1,5 @@
-xcopy %localappdata%\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets C:\Users\%Username%\Photos\"Windows Spotlight"
+xcopy %localappdata%\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets C:\Users\%Username%\Pictures\"Windows Spotlight"
+D
 cd \
 cd Users\%Username%\Pictures\"Windows Spotlight"
 ren * *.jpg
