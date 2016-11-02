554f81099fdc3cef5b3d5599c1e4281d8851846d
Jackson <jackson@borneman.com>
2016-11-02 08:48:57 -0400

    Added Deleted output

--- Copy Spotlight.bat
+++ Copy Spotlight.bat
@@ -1,3 +1,4 @@
+echo off
 xcopy %localappdata%\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets C:\Users\%Username%\Pictures\"Windows Spotlight"
 cd \
 cd Users\%Username%\Pictures\"Windows Spotlight"
@@ -6,8 +7,8 @@ echo "Hello"
 echo "Hello"
 ren * *.jpg
 FOR %%n in (Users\%Username%\Pictures\"Windows Spotlight" *.jpg) DO If %%~zn LSS 45000 (
+echo Deleted %%n
 del %%n
-echo Deleted
 )
 
 pause
