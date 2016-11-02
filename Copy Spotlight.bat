b0b543bc8710992aa96f3103296c8c3699d61353
Jackson <jackson@borneman.com>
2016-11-02 14:06:56 -0400

    Modified Xcopy and deleted mystery files leftover from Xcopy
    added Manual Pause

--- Copy Spotlight.bat
+++ Copy Spotlight.bat
@@ -1,14 +1,16 @@
 echo off
-xcopy %localappdata%\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets C:\Users\%Username%\Pictures\"Windows Spotlight"
+xcopy /q /y %localappdata%\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets C:\Users\%Username%\Pictures\"Windows Spotlight"
 cd \
 cd Users\%Username%\Pictures\"Windows Spotlight"
-echo "Hello"
-echo "Hello"
-echo "Hello"
 ren * *.jpg
-FOR %%n in (Users\%Username%\Pictures\"Windows Spotlight" *.jpg) DO If %%~zn LSS 45000 (
+Pause
+FOR %%n in (Users\%Username%\Pictures\"Windows Spotlight" *jpg) DO If %%~zn LSS 45000 (
 echo Deleted %%n
 del %%n
+)
+FOR %%a in (Users\%Username%\Pictures\"Windows Spotlight" *.) DO (
+echo Deleted %%a
+del %%a
 )
 
 pause
