5985b20e3fcdcfe4e823f699a2e92e3c666ca13e
Jackson <jackson@borneman.com>
2016-11-02 08:37:17 -0400

    Added Deleter and added Delay

--- Copy Spotlight.bat
+++ Copy Spotlight.bat
@@ -1,5 +1,13 @@
 xcopy %localappdata%\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets C:\Users\%Username%\Pictures\"Windows Spotlight"
-D
 cd \
 cd Users\%Username%\Pictures\"Windows Spotlight"
+echo "Hello"
+echo "Hello"
+echo "Hello"
 ren * *.jpg
+FOR %%n in (Users\%Username%\Pictures\"Windows Spotlight" *.jpg) DO If %%~zn LSS 30000 (
+del %%n
+echo Deleted
+)
+
+pause
