581e48197a04c2f091140b493a6e9001ac2d8e4d
Jackson <jackson@borneman.com>
2016-12-10 22:28:58 -0500

    Redirects files to onedrive

--- Copy Spotlight.bat
+++ Copy Spotlight.bat
@@ -1,13 +1,13 @@
 echo off
-xcopy /q /y %localappdata%\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets C:\Users\%Username%\Pictures\"Windows Spotlight"
+xcopy /q /y %localappdata%\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets C:\Users\%Username%\OneDrive\Pictures\"Windows Spotlight"
 cd \
-cd Users\%Username%\Pictures\"Windows Spotlight"
+cd Users\%Username%\OneDrive\Pictures\"Windows Spotlight"
 ren * *.jpg
-FOR %%n in (Users\%Username%\Pictures\"Windows Spotlight" *jpg) DO If %%~zn LSS 45000 (
+FOR %%n in (Users\%Username%\OneDrive\Pictures\"Windows Spotlight" *jpg) DO If %%~zn LSS 45000 (
 echo Deleted %%n
 del %%n
 )
-FOR %%a in (Users\%Username%\Pictures\"Windows Spotlight" *.) DO (
+FOR %%a in (Users\%Username%\OneDrive\Pictures\"Windows Spotlight" *.) DO (
 echo Deleted %%a
 del %%a
 )
